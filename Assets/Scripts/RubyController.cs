aa4c103e7d047f6c702a35cdc6f930061dccbafe
2200557yama <82437992+2200557yama@users.noreply.github.com>
2022-10-26 09:53:37 +0900

    1026v1

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,7 +11,10 @@ public class RubyController : MonoBehaviour
 
     public int maxHealth = 10; // 最大 HP
     private int currentHealth; // 現在の HP
-    public float speed = 1.0f;
+    //public float speed = 1.0f;
+
+    public Vector2 speed = new Vector2(0.0f, 0.0f);
+
 
     private Rigidbody2D rigidbody2d;
     private float horizontal;
@@ -49,6 +53,9 @@ public class RubyController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        speed.x = 10.0f * horizontal * Time.deltaTime;
+        speed.y = 10.0f * vertical * Time.deltaTime;
+
         // 無敵時間の更新処理
         //if (isInvincible)
         //{
@@ -66,11 +73,20 @@ public class RubyController : MonoBehaviour
     {
 
         Vector2 position = transform.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
+        //Vector2 position = rigidbody2d.AddForce(speed);
+        rigidbody2d.AddForce(speed);
+        
+
+        position.x = position.x + speed.x;
+        position.y = position.y + speed.y;
+
+
         // 物理システムに位置を伝える
         rigidbody2d.MovePosition(position);
     }
 
-    
+    private Vector2 Vector2(object p, float v)
+    {
+        throw new NotImplementedException();
+    }
 }
