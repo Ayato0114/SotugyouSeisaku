612a892b4be5c60ac34c5853b365c8bbc2175045
2200061-山道 <82436513+2200061-yamamichi@users.noreply.github.com>
2022-10-28 10:54:50 +0900

    1028(Clearに遷移)

--- Assets/Scripts/Tomoya/SceneSwitchClear.cs
+++ Assets/Scripts/Tomoya/SceneSwitchClear.cs
@@ -1,26 +1,26 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneSwitchClear : MonoBehaviour
 {
     //Exitに到達したかどうか
     private bool exitFlag = false;
 
+
     //Exitタイルに乗った時に呼び出される関数
-    private void OnTrggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D collider)
     {
         //まだ到達してない
-        if(!exitFlag)
+        if (!exitFlag)
         {
             //Playerタグの付いているオブジェクトが乗ったら
-            if(other.tag.Contains("Player"))
+            if (collider.tag.Contains("Player"))
             {
-                
-
                 //何度もフラグが立たないようにする
                 exitFlag = true;
+
+                SceneManager.LoadScene("Clear", LoadSceneMode.Single);
             }
         }
     }
-
-
 }
