f9729679f4162a064f5d06f646025ec73433f53b
2200557yama <82437992+2200557yama@users.noreply.github.com>
2022-10-28 12:29:34 +0900

    1028v2

--- Assets/Scripts/Takumi/YamaRubyController.cs
+++ Assets/Scripts/Takumi/YamaRubyController.cs
@@ -1,92 +1,112 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class YamaRubyController : MonoBehaviour
 {
-    //public float timeInvincible = 2.0f; // 無敵時間
-    //private bool isInvincible; // 無敵状態か
-    //private float invincibleTimer; // 残り無敵時間
-
-    public int maxHealth = 10; // 最大 HP
-    private int currentHealth; // 現在の HP
-    //public float speed = 1.0f;
-
-    public Vector2 speed = new Vector2(0.0f, 0.0f);
-
-
     private Rigidbody2D rigidbody2d;
     private float horizontal;
     private float vertical;
-    // Update()が呼ばれる前に一度だけ呼ばれる関数
+    public float speed = 3.0f;//移動速度
+    private Animator animator;
+    private Vector2 lookDirection = new Vector2(1, 0);
+
+    private bool isInvincible; //無敵状態か
+    private float invincibleTimer; //残り無敵時間
+
+    public float timeInvincible = 2.0f; //無敵時間
+    public int maxHealth = 5;   //最大HP
+    private int currentHealth;
+
+    // Start is called before the first frame update
+
+    public int health
+    {
+        get { return currentHealth; }
+    }
+
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-
-        // ゲーム開始時に HP を初期化する
         currentHealth = maxHealth;
-
-        //currentHealth = 1;
+        animator = GetComponent<Animator>();
     }
 
-    // 健康状態を変更する
+    // Update is called once per frame
+    void Update()
+    {
+        //FixedUpdate()ではinput関連の入力を読み取るのは
+        //禁止のため、update()で入力値を保存しておく
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        //入力値を代入
+        Vector2 move = new Vector2(horizontal, vertical);
+        //move.x、move.yの値が0.0fでない時は移動方向を設定する
+        //※入力値がない場合でも今向いてる方向を保持しておきたいため
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection = move;
+            lookDirection.Normalize();
+        }
+        //Animatorへパラメータを送信
+        animator.SetFloat("LookX", lookDirection.x);
+        animator.SetFloat("LookY", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        //Debug.Log(horizontal);//水平入力値をコンソールウィンドウへ出力
+        Vector2 position = transform.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+        transform.position = position;
+
+        // 無敵時間更新処理
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+            {
+                isInvincible = false;
+            }
+        }
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = transform.position;
+
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        //物理システムに位置を伝える
+        rigidbody2d.MovePosition(position);
+
+    }
+
+
     public void ChangeHealth(int amount)
     {
-        //if (amount < 0)
-        //{
-        //    if (isInvincible) return;
-        //    isInvincible = true;
-        //    invincibleTimer = timeInvincible;
-        //}
-
-        // 現在の HP を 0～maxHealth の間に収まるように変更する
+        if (amount < 0)
+        {
+            if (isInvincible) return;
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
 
-    // 毎フレーム呼ばれる関数
-    void Update()
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        // FixedUpdate()では Input 関連の入力を読み取るのは
-        // 禁止のため、Update()で入力値を保存しておく
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+        EnemyController enemy_controllers = collision.GetComponent<EnemyController>();
+        if (enemy_controllers != null)
+        {
+            enemy_controllers.ChangeHealth(-1);
 
-        speed.x = 15.0f * horizontal * Time.deltaTime;
-        speed.y = 15.0f * vertical * Time.deltaTime;
-
-        // 無敵時間の更新処理
-        //if (isInvincible)
-        //{
-        //    invincibleTimer -= Time.deltaTime;
-        //    if (invincibleTimer < 0)
-        //    {
-        //        isInvincible = false;
-        //    }
-        //}
-
-    }
-
-    // 一定時間毎に呼ばれる関数
-    void FixedUpdate()
-    {
-
-        Vector2 position = transform.position;
-        //Vector2 position = rigidbody2d.AddForce(speed);
-        rigidbody2d.AddForce(speed);
-
-
-        position.x = position.x + speed.x;
-        position.y = position.y + speed.y;
-
-
-        // 物理システムに位置を伝える
-        rigidbody2d.MovePosition(position);
-    }
-
-    private Vector2 Vector2(object p, float v)
-    {
-        throw new NotImplementedException();
+            if (enemy_controllers.health == 0)
+            {
+                Destroy(enemy_controllers.gameObject);
+            }
+        }
     }
 }
+
+
