257a71942fd662b3759560e2d2733b6958a52e56
Ayato0114 <ayato.0114_@i.softbank.jp>
2022-11-18 03:05:15 +0900

    MAP　攻撃

--- Assets/Scripts/Ayato/Projectile.cs
+++ Assets/Scripts/Ayato/Projectile.cs
@@ -5,18 +5,33 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     // Start is called before the first frame update
+    
     private Rigidbody2D rigidbody2d;
-    [SerializeField] private Transform playerTrans; //追いかける対象のTransform
+
+    private GameObject playerObject;
+    private Vector3 playerTrans ; //追いかける対象のTransform
     [SerializeField] private float bulletSpeed;  　 //弾の速度
     [SerializeField] private float limitSpeed;      //弾の制限速度
     private Transform bulletTrans;                  //弾のTransform
+
 
     [SerializeField] public float deleteTime;//弾の時間
     float time = 0f;
 
     void Awake()
     {
-      
+        playerObject = GameObject.FindWithTag("Enemy");
+        if (playerObject == null)
+        {
+            playerTrans.x = 1;
+            playerTrans.y = 1;
+            playerTrans.z = 1;
+        }
+        else
+        {
+            playerTrans = playerObject.transform.position;
+        }
+       
         rigidbody2d = GetComponent<Rigidbody2D>();
         bulletTrans = GetComponent<Transform>();
         time = 0;
@@ -36,15 +51,35 @@ public class Projectile : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        if (playerObject == null)
+        {
+            Destroy(gameObject);
+        }
     }
+
     private void FixedUpdate()
     {
-         Vector3 vector3 = playerTrans.position - bulletTrans.position;         //弾から追いかける対象への方向を計算
-        rigidbody2d.AddForce(vector3.normalized * bulletSpeed);                 //方向の長さを1に正規化、任意の力をAddForceで加える
 
-        float speedXTemp = Mathf.Clamp(rigidbody2d.velocity.x, -limitSpeed, limitSpeed);　//X方向の速度を制限
-        float speedYTemp = Mathf.Clamp(rigidbody2d.velocity.y, -limitSpeed, limitSpeed);  //Y方向の速度を制限
-        rigidbody2d.velocity = new Vector3(speedXTemp, speedYTemp);　　　　　　　　　　　//実際に制限した値を代入
+        if (playerObject == null)
+        {
+            Destroy(gameObject);
+        }
+
+
+        if (playerObject != null)
+        {
+
+            Vector3 vector3 = playerTrans - bulletTrans.position;         //弾から追いかける対象への方向を計算
+            rigidbody2d.AddForce(vector3.normalized * bulletSpeed);                 //方向の長さを1に正規化、任意の力をAddForceで加える
+
+            float speedXTemp = Mathf.Clamp(rigidbody2d.velocity.x, -limitSpeed, limitSpeed); //X方向の速度を制限
+            float speedYTemp = Mathf.Clamp(rigidbody2d.velocity.y, -limitSpeed, limitSpeed);  //Y方向の速度を制限
+            rigidbody2d.velocity = new Vector3(speedXTemp, speedYTemp);           //実際に制限した値を代入
+
+        }
+    
+       
     }
 
     public void Launch(Vector2 direction, float force)
