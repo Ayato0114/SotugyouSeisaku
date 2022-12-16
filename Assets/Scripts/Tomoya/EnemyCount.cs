686caa5d8d398f21ff7cca93032924c9989d5599
2200061-山道 <82436513+2200061-yamamichi@users.noreply.github.com>
2022-12-16 10:42:59 +0900

    12/26

--- Assets/Scripts/Tomoya/EnemyCount.cs
+++ Assets/Scripts/Tomoya/EnemyCount.cs
@@ -5,18 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class EnemyCount : MonoBehaviour
 {
+    //シングルトン用変数
     public static EnemyCount instance;
 
+    //初期設定
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
             DontDestroyOnLoad(this.gameObject);
+
+            Debug.Log("シングルトン生成");
         }
         else
         {
             Destroy(this.gameObject);
+            Debug.Log("シングルトン破棄");
         }
     }
 
@@ -25,8 +30,12 @@ public class EnemyCount : MonoBehaviour
 
     //敵が全滅したときに立つフラグ
     public static bool annihilatedEnemy = false;
-   
 
+    //現在の敵の数
+    public static int enemyCount = 0;
+
+
+    //更新処理
     void Update()
     {
         enemyBox = GameObject.FindGameObjectsWithTag("Enemy");
@@ -34,7 +43,15 @@ public class EnemyCount : MonoBehaviour
         if (enemyBox.Length == 0)
         {
             annihilatedEnemy = true;
+            Debug.Log("敵全滅");
         }
+        if(enemyBox.Length != enemyCount)
+        {
+            Debug.Log("敵の数:" + enemyBox.Length.ToString());
+        }
+
+        //配列数を記憶しておく
+        enemyCount = enemyBox.Length;
     }
 
     bool GetAnnihilatedEnemy()
